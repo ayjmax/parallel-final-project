12a96735f6066d4665e90b6d2d501dbccb9547f8
Alec <ayj.max@gmail.com>
2025-04-26 15:36:07 -0400

    Updated runner for kmeans

--- run_timing_kmeans.sh
+++ run_timing_kmeans.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 # Check if all required arguments are provided
-if [ $# -ne 2 ]; then
+if [ $# -ne 3 ]; then
     echo "Error: Missing arguments"
-    echo "Usage: $0 <homework_name> <model_name> <version>"
+    echo "Usage: $0 <model_name> <version> <dataset ('exchange' or 'drybean')>"
     exit 1
 fi
 
-TESTING_HW="kmeans" # Get first argument as homework name
-TESTING_MODEL=$1 # Get second argument as model name
+TESTING_HW="kmeans"
+TESTING_MODEL=$1
 VERSION=$2
-OUTPUT_CSV_FILE="results/$TESTING_HW/$TESTING_MODEL-$VERSION.csv" # Output CSV file
-OUTPUT_TXT_FILE="results/$TESTING_HW/$TESTING_MODEL-$VERSION.txt" # Output TXT file
+DATASET=$3
+OUTPUT_CSV_FILE="results/$TESTING_HW/$TESTING_MODEL-$VERSION-$DATASET.csv" # Output CSV file
+OUTPUT_TXT_FILE="results/$TESTING_HW/$TESTING_MODEL-$VERSION-$DATASET.txt" # Output TXT file
 
 # Clear CSV, write header
 echo "Implementation,Dataset,Time(ms)" > "$OUTPUT_CSV_FILE"
-echo "testing $TESTING_MODEL"
+echo "testing $TESTING_MODEL with $DATASET"
 
 # Clear TXT
 echo "" > "$OUTPUT_TXT_FILE"
@@ -31,25 +32,37 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Loop over all .txt files in the specified directory
-for dataset in /proj/cse375-475/exchange/datasets/*.txt; do
-    debug_message="[DEBUG] Running $TESTING_MODEL-$TESTING_HW-$VERSION with: $dataset"
-    echo "$debug_message"
+if [ "$DATASET" == "exchange" ]; then
+    # Loop over all .txt files in the specified directory
+    for dataset in /proj/cse375-475/exchange/datasets/*.txt; do
+        debug_message="[DEBUG] Running $TESTING_MODEL-$TESTING_HW-$VERSION with: $dataset"
+        echo "$debug_message"
 
+        # Run the program with the dataset as input
+        OUTPUT=$(cat "$dataset" | ./bin/kmeans/${TESTING_MODEL}-${VERSION})
+
+        # Write to TXT
+        echo "$debug_message" >> "$OUTPUT_TXT_FILE"
+        echo "$OUTPUT" >> "$OUTPUT_TXT_FILE"
+
+        # Extract the total time from the output and append to the CSV file
+        total_time=$(echo "$OUTPUT" | grep 'Total time:'    | cut -d' ' -f3)
+        echo "$TESTING_MODEL,$(basename "$dataset"),$total_time" >> "$OUTPUT_CSV_FILE"
+
+        # Print to terminal
+        echo "$OUTPUT"
+    done
+elif [ "$DATASET" == "drybean" ]; then
     # Run the program with the dataset as input
-    OUTPUT=$(cat "$dataset" | ./bin/kmeans/${TESTING_MODEL}-${VERSION})
+    OUTPUT=$(cat "$DATASET" | ./bin/kmeans/${TESTING_MODEL}-${VERSION})
+
+    # Write to CSV
+    total_time=$(echo "$OUTPUT" | grep 'Total time:'    | cut -d' ' -f3)
+    echo "$TESTING_MODEL,$DATASET,$total_time" >> "$OUTPUT_CSV_FILE"
 
     # Write to TXT
     echo "$debug_message" >> "$OUTPUT_TXT_FILE"
     echo "$OUTPUT" >> "$OUTPUT_TXT_FILE"
-
-    # Extract the total time from the output and append to the CSV file
-    total_time=$(echo "$OUTPUT" | grep 'Total time:'    | cut -d' ' -f3)
-    echo "${TESTING_MODEL},$(basename "$dataset"),$total_time" >> "$OUTPUT_CSV_FILE"
-
-    # Print to terminal
-    echo "$OUTPUT"
-done
+fi
 
 echo "All tests completed. Results saved in $OUTPUT_CSV_FILE and $OUTPUT_TXT_FILE"
-
